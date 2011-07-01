e12f1023a832489e957deefaab709c01b79a4bd4
Felipe Sampaio <felipe.sampaio@inf.ufrgs.br>
2011-07-01 13:44:02 -0300

    Control Unit Module

---
+++ arch/decode.cpp
@@ -0,0 +1,12 @@
+#include "decode.h"
+
+void decode::compute() {
+	opcode.write(word.read().range(31,26));
+	rs.write(word.read().range(25,21));
+	rt.write(word.read().range(20,16));
+	rd.write(word.read().range(15,11));
+	shamt.write(word.read().range(10,6));
+	funct.write(word.read().range(5,0));
+	immediate.write(word.read().range(15,0));
+	address.write(word.read().range(25,0));
+}
