228609a214bc1f2d63eac4b245e16843d2723696
Daniel Palomino <dmvpalomino@inf.ufrgs.br>
2011-07-03 01:46:29 -0300

    estagio de execucao quase pronto e estagio de store iniciado

--- arch/data_path.h
+++ arch/data_path.h
@@ -41,11 +41,14 @@ SC_MODULE(data_path) {
 	sc_signal<uint<32> > in1_pc_mux;//vem do alem TODO
 
 	//ID STAGE
+	//INSTANCIAS
 	decode *decoder;
 	mux2_5b *breg_mux;
 	breg *bank_reg;
 	sbreg *spec_bank_reg;
 	demux4x2 *demux_breg;
+	reg_32b *a_reg;
+	reg_32b *b_reg;
 
 	//SINAIS
 	sc_signal<sc_uint<6> > opcode;
@@ -63,10 +66,43 @@ SC_MODULE(data_path) {
 	sc_signal<sc_logic > sbreg_we;
 	sc_signal<sc_uint<32> > out_sbreg_a;
 	sc_signal<sc_uint<32> > out_sbreg_b;
+	
+	sc_signal<sc_uint<32> > out_demux_a;
+	sc_signal<sc_uint<32> > out_demux_b;
+
+	sc_signal<sc_uint<32> > out_a_reg;
+	sc_signal<sc_uint<32> > out_b_reg;
 
 	//EXE STAGE
+	//INSTANCIAS
+	alu *ula;
+	mac *mac;
+	reg_32b *ula_reg;
+	reg_32b *mac_reg;
+
+	//SINAIS
+	sc_signal<sc_uint<32> > out_mux_op_a;
+	sc_signal<sc_uint<32> > out_mux_op_b;
+	sc_signal<sc_uint<32> > out_ula;
+	sc_signal<sc_logic> out_flag_z;
+
+	sc_signal<sc_uint<32> > out_ula_reg;
+
+	sc_signal<sc_uint<32> > out_mac;
+	
+	sc_signal<sc_uint<32> > out_mac_reg;
+
+
 
 	//MEM STAGE
+	//INSTANCIAS
+	memData *memData;
+	reg_32b *memData_reg_a;
+	reg_32b *memData_reg_b;
+
+	//SINAIS
+	sc_signal<uint<32> > out_memData_a;
+	sc_signal<uint<32> > out_memData_b;
 
 	//WB STAGE
 
@@ -145,9 +181,51 @@ SC_MODULE(data_path) {
 		demux_breg->s0(out_demux_a);
 		demux_breg->s1(out_demux_b);
 		
+		a_reg = new reg_32b("op_a_reg");
+		a_reg->clk(clk);
+		a_reg->rst(reset);
+		a_reg->d(out_demux_a);
+		a_reg->q(out_a_reg);
 
+		b_reg = new reg_32b("op_b_reg");
+		b_reg->clk(clk);
+		b_reg->rst(reset);
+		b_reg->d(out_demux_b);
+		b_reg->q(out_b_reg);
 
+		//EXE STAGE
+		ula = new alu("ula");
+		ula->funct(ceu); //vem do controle TODO
+		ula->op_a(out_mux_op_a);
+		ula->op_b(out_mux_op_b);
+		ula->alu_out(out_ula);
+		ula->z(out_flag_z);
 
+		ula_reg = new reg_32b("ula_reg");
+		ula_reg->clk(clk);
+		ula_reg->rst(reset);
+		ula_reg->d(out_ula);
+		ula_reg->q(out_ula_reg);
+
+		mac = new mac("mac");
+		mac->input0(xxx); //vem da saida da memoria de dados TODO
+		mac->input1(yyy); //vem da saida da memoria de dados TODO
+		mac->inputreg(zzz); //entrada que será somada, provavelmente vem da saida de um mux TODO
+		mac->output(out_mac);
+
+		mac_reg = new reg_32b("mac_reg");
+		mac_reg->clk(clk);
+		mac_reg->rst(reset);
+		mac_reg->d(out_mac);
+		mac_reg->q(out_mac_reg);
+
+		//MEM STAGE
+		memData = new memData("memData");
+		memData->adress(xxx); //vem da saida do mux da entrada
+		memData->we(memData_we);
+		memData->re(memData_re);
+		//PAREI AQUI
+		//TEM QUE REVER A QTDADE DE PORTAS DE ENTRADA E SAIDA DA MEMORIA DE DADOS
 
 
 
